45c2b19697625a97139918b19e89297adc27ccb0
Jorge Silva <jorgecarrilloit@gmail.com>
2024-12-25 04:27:10 +0000

    Add base about and blog pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>Information about the company goes here...</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <p>All blog posts will be listed here...</p>
+    </Layout>
+  );
+};
+
+export default BlogPage;
